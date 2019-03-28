39e46a8065b3f3547edb598359596a50fadb7181
sssergy <admin@rem05.ru>
2019-03-28 18:53:13 +0300

    Update win64QT_compile.sh

--- win64QT_compile.sh
+++ win64QT_compile.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 sudo make clean
 chmod 777 -R *
-apt-get install libssl1.0-dev
 PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g')
 cd `pwd`/depends
-sudo make -j4 HOST=x86_64-w64-mingw32 --enable-tests=no
+sudo make -j6 HOST=x86_64-w64-mingw32
 cd ..
 sudo ./autogen.sh
 mkdir db4
@@ -12,10 +11,10 @@ wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
 tar -xzvf db-4.8.30.NC.tar.gz
 cd db-4.8.30.NC/build_unix/
 ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
-sudo make install
+sudo make -j6 install
 cd ../../
 sudo ./autogen.sh
 ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-w64-mingw32 --enable-tests=no
-sudo make -j4
+sudo make -j6
 strip src/*.exe
 strip src/qt/*.exe
