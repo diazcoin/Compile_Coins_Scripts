f6b943e841670f5cffc6bf5855def03866a002c6
sssergy <admin@rem05.ru>
2019-04-11 13:08:21 +0300

    Update linux64_nogui.sh

--- linux64_nogui.sh
+++ linux64_nogui.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 sudo make clean
 chmod 777 -R *
-sudo apt-get update
-sudo apt-get -y upgrade
 cd `pwd`/depends
-sudo make -j2
+sudo make -j6
 cd ..
 sudo ./autogen.sh
 mkdir `pwd`/db4
@@ -15,6 +13,6 @@ cd `pwd`/db-4.8.30.NC/build_unix/
 sudo make install
 cd ../../
 sudo ./autogen.sh
-./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-pc-linux-gnu
-sudo make -j2
+./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-pc-linux-gnu --enable-tests=no
+sudo make -j6
 echo "Remember to strip the daemon, cli, and tx files!"
