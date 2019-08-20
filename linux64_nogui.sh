226ec7de7dec5309539688376b1fcdedaf3d6eff
sssergy <admin@rem05.ru>
2019-08-20 06:25:57 +0300

    remove sudo

--- linux64_nogui.sh
+++ linux64_nogui.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-sudo make clean
+make clean
 chmod 777 -R *
 cd `pwd`/depends
-sudo make -j6
+make -j6
 cd ..
-sudo ./autogen.sh
+./autogen.sh
 mkdir `pwd`/db4
 wget -c 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
 tar -xzvf db-4.8.30.NC.tar.gz
 cd `pwd`/db-4.8.30.NC/build_unix/
 ../dist/configure --enable-cxx --disable-shared --with-pic --prefix=`pwd`/db4
-sudo make install
+make install
 cd ../../
-sudo ./autogen.sh
+./autogen.sh
 ./configure LDFLAGS="-L`pwd`/db4/lib/" CPPFLAGS="-I`pwd`/db4/include/" --prefix=`pwd`/depends/x86_64-pc-linux-gnu --enable-tests=no
-sudo make -j6
+make -j6
 echo "Remember to strip the daemon, cli, and tx files!"
